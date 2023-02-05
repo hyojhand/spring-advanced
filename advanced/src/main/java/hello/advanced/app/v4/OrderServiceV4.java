554f4a87a86d0ccf66bc5852a054e0c80989d825
손효재 <sonjw5128@gmail.com>
2023-02-05 23:50:02 +0900

    feat: template method pattern

---
+++ advanced/src/main/java/hello/advanced/app/v4/OrderServiceV4.java
@@ -0,0 +1,27 @@
+package hello.advanced.app.v4;
+
+import hello.advanced.app.v5.OrderRepositoryV5;
+import hello.advanced.trace.logtrace.LogTrace;
+import hello.advanced.trace.template.AbstractTemplate;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class OrderServiceV4 {
+
+    private final OrderRepositoryV5 orderRepository;
+    private final LogTrace trace;
+
+    public void orderItem(String itemId) {
+        AbstractTemplate<Void> template = new AbstractTemplate(trace) {
+            @Override
+            protected Object call() {
+                orderRepository.save(itemId);
+                return null;
+            }
+        };
+        template.execute("OrderService.orderItem()");
+    }
+
+}
